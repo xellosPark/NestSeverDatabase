7e5b851715f12c8f848e562197876996f6cdd9b7
ParkDongJun <yuki@ubisam.com>
2025-01-10 16:13:29 +0900

    머지 완료료

--- server-app/src/post/post.controller.ts
+++ server-app/src/post/post.controller.ts
@@ -12,6 +12,12 @@ export class PostController {
     //     return this.postService.getPosts();
     // }
 
+// ParseIntPipe는 다음과 같은 상황에서 매우 유용합니다:
+
+// URL 파라미터 값을 정수형으로 자동 변환해야 할 때.
+// 값이 유효한 숫자인지 검증해야 할 때.
+// 추가적인 파이프를 정의하거나 사용할 수도 있으며, NestJS에서는 이를 확장하거나 커스터마이징할 수 있습니다.
+
     @Get('/posts/:id')
     getPostById(@Param('id', ParseIntPipe) id: number){
         return this.postService.getPostById(id);
